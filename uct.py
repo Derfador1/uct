0db69f0a4d640fcb2308a19a5389215cd3645b30
Derfador1 <Derfador1@gmail.com>
2016-06-24 14:29:51 -0400

    moved over to python first commit for working connections to server

---
+++ uct.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python3
+
+import socket
+import threading
+import select
+
+#basis acquired on 
+#http://www.tutorialspoint.com/python/python_multithreading.htm
+class myThread (threading.Thread):
+	def __init__(self, threadID, name, sock):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.sock = sock
+	def run(self):
+		print("Starting " + self.name)
+		poller(self.sock)
+
+def poller(sock):
+	#copied from https://pymotw.com/2/select/ and with help of SPC Primm
+	READ_ONLY = select.POLLIN | select.POLLPRI | select.POLLHUP | select.POLLERR
+	TIMEOUT = 200
+	poller = select.poll()
+	poller.register(sock, READ_ONLY)
+	fd_to_socket = { sock.fileno(): sock,}
+	while True:
+	# Wait for at least one of the sockets to be ready for processing
+		events = poller.poll(TIMEOUT)
+		for fd, flag in events:
+		# Retrieve the actual socket from its file descriptor
+			s = fd_to_socket[fd]
+			if flag & (select.POLLIN | select.POLLPRI):
+				PONG(sock)
+
+def user_create(sock):
+	user = "user stephen james iracane reed\n"
+	nick = "nick samuels\n"
+	sock.send(bytes(user, 'utf-8'))
+	sock.send(bytes(nick, 'utf-8'))
+	x = sock.recv(1024)
+	print(x.decode('utf-8'))
+
+#ping response found with the help of SPC Primm
+def PONG(sock):
+	print("Enter a command: ")
+	x = sock.recv(1024)
+	x = x.decode("utf-8")
+	print(x)
+	if "PING" in x:
+		sock.send(bytes("PONG " + x[6:] + '\n', 'utf-8'))
+	print("Enter a command: ")
+
+def send_msg(sock, message):
+	message = message.split(" ", 1)
+	sock.send(bytes("privmsg " + message[0] + " :" + message[1] + "\n", "utf-8"))
+
+def send_help(sock):
+	sock.send(bytes("help\n", "utf-8"))
+	x = sock.recv(1024)
+	x = x.decode("utf-8")
+	print(x)
+
+def main():
+	commands = {
+		"AWAY":None, "ISON":None, "HELP":send_help, "INFO":send_help,
+		"JOIN":None, "LIST":None, "LUSERS":None, "MODE":None, "MOTD":None, "NICK":None, 
+		"NOTICE":send_msg, "PART":None, "PING":None, "PONG":None, "PRIVMSG":send_msg, "QUIT":None, 
+		"TOPIC":None, "WALLOPS":None, "WHO":None, "WHOIS":None
+	}
+
+	sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sd.connect(("127.0.0.1", 6667))
+	user_create(sd)
+	thread = myThread(1, "Thread 1", sd)
+	thread.start()
+	channel = None
+	while True:
+		x = input("Enter a command: ")
+		character = x.split(" ", 1)
+		command = character[0].upper()
+		if command in commands.keys():
+			print(command)
+			if command == 'JOIN':
+				channel = character[1]
+				commands[command](sd, character[1])
+			elif command == 'HELP':
+				commands[command](sd)
+		elif channel:
+			commands["PRIVMSG"](sock, (channel + " " + x))
+
+
+
+if __name__ == "__main__":
+	main()
