aa7bde36d0791fd1312376b5dad3951adcc64c3f
Derfador1 <Derfador1@gmail.com>
2016-06-24 12:34:12 -0400

    changed to a working copy with forking

--- uct.c
+++ uct.c
@@ -7,10 +7,12 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/timeb.h>
+#include <sys/wait.h>
 
 #define SIZE_RECV 1024
 #define SIZE_SENT 512
@@ -42,27 +44,80 @@ int main(void)
 	
 	printf("We have successfully made a connection\n");
 
-	sleep(1);
-	for(;;)
-	{
-		ssize_t msg_sz = 0;
-		ssize_t snd_sz = 0;
+	pid_t b;
 
-		while((msg_sz = recv(sd, &recieved, sizeof(recieved)-1, MSG_DONTWAIT)) > 0) {
-			recieved[msg_sz] = '\0';
-			printf("%s", recieved);
+	b = fork();
+
+	ssize_t msg_sz = 0;
+	ssize_t snd_sz = 0;
+
+	if(b < 0) {
+		perror("Fork failed");
+	}
+	else if(b == 0) {
+		for(;;)
+		{
+			while((msg_sz = recv(sd, &recieved, sizeof(recieved)-1, MSG_DONTWAIT)) > 0) {
+				recieved[msg_sz] = '\0';
+				printf("%s", recieved);
+			}
+
+			memset(recieved, '\0', sizeof(recieved));
 		}
+	}
+	else {
+		//user
+		//nick
+		for(;;) {
+			sleep(1);
 
-		printf(">");
-		fgets(sent, sizeof(sent), stdin);
-		if ((snd_sz = send(sd, &sent, strlen(sent), 0)) != (ssize_t)strlen(sent)) {
-			perror("Message transmission error");
+			if(msg_sz > 0) {
+				recieved[msg_sz] = '\0';
+				printf("here\n");
+				if(strstr(recieved, "PING") != NULL) {
+					memset(sent, '\0', sizeof(recieved));
+					char *pos = malloc(sizeof(pos));
+					pos = strstr(recieved, " ")+1;
+					sprintf(sent, "PONG %s\r\n", pos);
+					if ((snd_sz = send(sd, &sent, strlen(sent), 0)) != (ssize_t)strlen(sent)) {
+						perror("Message transmission error");
+					}
+				}
+			}
+
+			printf("What would you like to do: ");
+			fgets(sent, sizeof(sent), stdin);
+
+			sent[strlen(sent)] = '\0';
+
+			if ((snd_sz = send(sd, &sent, strlen(sent), 0)) != (ssize_t)strlen(sent)) {
+				perror("Message transmission error");
+			}
+
+			if(msg_sz > 0) {
+				recieved[msg_sz] = '\0';
+				printf("here\n");
+				if(strstr(recieved, "PING") != NULL) {
+					memset(sent, '\0', sizeof(recieved));
+					char *pos = malloc(sizeof(pos));
+					pos = strstr(recieved, " ")+1;
+					sprintf(sent, "PONG %s\r\n", pos);
+					if ((snd_sz = send(sd, &sent, strlen(sent), 0)) != (ssize_t)strlen(sent)) {
+						perror("Message transmission error");
+					}
+				}
+			}
+
+			if(strncmp(sent, "quit\n", strlen(sent)) == 0) {
+				printf("Exitting...");
+				kill(b, SIGKILL);
+				exit(0);
+			}	
+
+			memset(sent, '\0', sizeof(recieved));
+
 		}
-
-		memset(recieved, '\0', SIZE_RECV);
-		memset(sent, '\0', SIZE_SENT);
 	}
 
-	free(recieved);
-	free(sent);
+	close(sd);
 }
