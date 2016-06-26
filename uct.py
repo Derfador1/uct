fe21dedb47ac19fe53fa1d052a6193d5ce666a72
Derfador1 <Derfador1@gmail.com>
2016-06-26 14:44:51 -0400

    added comments and final touch up work

--- uct.py
+++ uct.py
@@ -60,15 +60,18 @@ def pong_handle(sock):
 			
 #basic outline for how to send message acquired from 
 #https://github.com/dsprimm/uct/blob/master/uct.py
-	
+
+#handles the message sending	
 def send_msg(session, message):
 	message = message.split(" ", 1)
 	session.sock.send(bytes("privmsg " + message[0] + " : " + message[1] + "\n", "utf-8"))
 	print("\nEnter a command: ", end="")
 
+#handles help
 def send_help(session, void):
 	session.sock.send(bytes("help\n", "utf-8"))
 
+#handles quit
 def send_quit(session, void):
 	session.worker.running[0] = 0
 	session.worker.join()
@@ -78,6 +81,7 @@ def send_quit(session, void):
 #logic and structure found from working with
 #SPC Primm
 #start
+#handles switching channels
 def channel_switch(session, channel):
 	if channel[0] != "#":
 		channel = "#" + channel
@@ -86,6 +90,7 @@ def channel_switch(session, channel):
 	session.channel = channel
 	session.sock.send(bytes("join " + channel + "\n", "utf-8"))
 
+#handles going afk in a channel
 def away(session, message):
 	if message:
 		command = message.split(" ", 1)
@@ -96,9 +101,11 @@ def away(session, message):
 	else:
 		session.sock.send(bytes("away\n", "utf-8"))
 
+#handles who is on
 def ison(session, person):
 	session.sock.send(bytes("ison "+person+"\n", "utf-8"))	
 
+#handles leaving a channel
 def leave(session, message):
 	if message[0] != "#":
 		message = "#" + message
@@ -108,9 +115,12 @@ def leave(session, message):
 		message = command[0] + " " + command[1]
 	session.sock.send(bytes("part " + message + "\n", "utf-8"))
 #end
+
+#handles the who command to the server
 def who(session, channel):
 	session.sock.send(bytes("who " + channel + "\n", "utf-8"))
 
+#handles chaning a nickname
 def change_name(session, new_name):
 	if new_name:
 		if len(new_name) > 0 and len(new_name) <= 9:
@@ -119,11 +129,13 @@ def change_name(session, new_name):
 			print("The length of the new username must be less than 9 characters long.\n")
 	else:
 		print("\nEnter a command: ", end="")
-			
+
+#handles pong			
 def pong(session, void):
 	print("\nEnter a command: ", end="")
 	pass
 
+#handles sending ping
 def ping(session, target):
 	if target:
 		if len(target) <= 9:
@@ -132,7 +144,8 @@ def ping(session, target):
 			print("Invalid target given")
 	else:
 		print("\nEnter a command: ", end="")
-			
+
+#handles querying a nickname			
 def who_is(session, name):
 	if name:
 		if len(name) > 0 and len(name) <= 9:
@@ -142,13 +155,15 @@ def who_is(session, name):
 	else:
 		print("\nEnter a command: ", end="")
 
-			
+#handles giving the user the message of the day			
 def motd(session, void):
 	session.sock.send(bytes("motd\n", "utf-8"))	
 	
+#handles giving the user a list of other users
 def lusers(session, void):
 	session.sock.send(bytes("lusers\n", "utf-8"))
-	
+
+#handles listing who is in a channel	
 def list1(session, channel):
 	if channel:
 		if channel[0] != "#":
@@ -157,7 +172,7 @@ def list1(session, channel):
 	else:
 		session.sock.send(bytes("list\n", "utf-8"))
 		
-		
+#handles changing or seeing a topic of a channel		
 def topic(session, channel):
 	if channel:
 		if channel[0] != "#":
@@ -173,7 +188,8 @@ def topic(session, channel):
 		session.sock.send(bytes("topic "+ channel + "\n", "utf-8"))
 	else:
 		print("\nEnter a command: ", end="")
-		
+	
+#handles broadcasting a message	
 def broadcast(session, message):
 	if message[0] != ":":
 		message = ":" + message
@@ -191,6 +207,8 @@ def main():
 		"TOPIC":topic, "WALLOPS":broadcast, "WHO":who, "WHOIS":who_is
 	}
 
+	#sets up my socket connection and thread
+	#start
 	sd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sd.connect(("127.0.0.1", 6667))
 	session = status()
@@ -198,15 +216,19 @@ def main():
 	user_create(sd)
 	session.worker = myThread(sd)
 	session.worker.start()
+	#end
 	print("Enter a command: ", end="")
 	while True:
 		try:
+			#takes input and makes sure there is input
 			x = input()
 			if len(x) > 0:
+				#makes sure input starts with '/'
 				if x[0] == '/':
 					c = x.split(" ", 1)
 					c[0] = c[0][1:].upper()
 					command = c[0]
+					#sends command based on length of given input
 					if(len(c) > 1):
 						if command in commands.keys():
 							commands[command](session, c[1])
@@ -219,7 +241,8 @@ def main():
 				print("Please enter something valid")
 				print("Enter a command: ", end="")
 		except KeyboardInterrupt:
-			print("Keyboard Interrupt caught..")
+			#catches keyboard interrupt and closes gracefully
+			print("\nKeyboard Interrupt caught..")
 			session.worker.running[0] = 0
 			session.worker.join()
 			session.sock.close()
